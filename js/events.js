41223a9326c7176d563f921ef1e154ea2168866f
RSNF <ray1001novaes@gmail.com>
2024-04-09 18:58:52 -0300

    feat: js scripts to the new functions

--- js/events.js
+++ js/events.js
@@ -1,13 +1,34 @@
-import { drawGrid, clearGrid, gridContainer, gridInput } from "./grid-utils.js";
+import { drawGrid, clearGrid, gridContainer, gridInput, colors } from "./grid-utils.js";
 
-gridContainer.addEventListener("mouseover", (e) => {
+const gridHover = document.querySelector("#gridHover");
+const gridRainbow = document.querySelector("#gridRainbow");
+const colorsDiv = document.querySelector(".colors");
+let currentColor = colors[0];
+
+function mouseColoring(e) {
     switch (e.target.className) {
         case "grid-item":
-            e.target.style.backgroundColor = "black";
-            break;
 
-        default:
-            break;
+            if(gridRainbow.checked) {
+                let number = Math.floor(Math.random() * 12);
+                currentColor = colors[number];
+            }
+
+            e.target.style.backgroundColor = currentColor;
+        break;
+
+    default:
+        break;
+    }
+}
+
+gridHover.addEventListener("click", () => {
+    if (gridHover.checked) {
+        gridContainer.removeEventListener("click", mouseColoring);
+        gridContainer.addEventListener("mouseover", mouseColoring)
+    } else {
+        gridContainer.removeEventListener("mouseover", mouseColoring);
+        gridContainer.addEventListener("click", mouseColoring)
     }
 });
 
@@ -16,3 +37,16 @@ gridInput.addEventListener("input", () => {
     clearGrid();
     drawGrid(grid);
 });
+
+colorsDiv.addEventListener("click", (e) => {
+    switch (e.target.className) {
+        case "color":
+            currentColor = e.target.style.backgroundColor;
+            break;
+
+        default:
+            break;
+    }
+});
+
+gridContainer.addEventListener("click", mouseColoring)
--- js/grid-utils.js
+++ js/grid-utils.js
@@ -1,5 +1,12 @@
 const gridContainer = document.querySelector(".grid-container");
-const gridInput = document.querySelector("input");
+const gridInput = document.querySelector("#gridNum");
+
+const colors = [
+    "#000002", "#1d2b53", "#7e2553", "#008751",
+    "#ab5236", "#5f574f", "#c2c3c7", "#fff1e8",
+    "#ff004d", "#ffa300", "#ffec27", "#00e436",
+    "#29adff", "#83769c", "#ff77a8", "#ffccaa"
+];
 
 function drawGrid(grid) {
     for (let i = 0; i < grid*grid; i++) {
@@ -20,6 +27,24 @@ function clearGrid() {
     }
 }
 
+function addColors() {
+    const colorsDiv = document.querySelector(".colors");
+
+    for (let i = 0; i < colors.length; i++) {
+        let color = document.createElement("div");
+        color.className = "color"
+        color.style.backgroundColor = colors[i];
+        color.style.width = 500 / 12 + "px";
+        color.style.height = 500 / 12 + "px";
+        colorsDiv.appendChild(color);
+    }
+}
+
+addColors();
+
 drawGrid(16);
 
-export { drawGrid, clearGrid, gridContainer, gridInput };
+export {
+    drawGrid, clearGrid,
+    gridContainer, gridInput, colors
+};
